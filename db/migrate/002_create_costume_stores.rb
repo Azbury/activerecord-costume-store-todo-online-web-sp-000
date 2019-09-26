f9ed7d74707e4ad237af3f399b52c1350cc92253
Austin Asbury <acasbury25@aol.com>
2019-09-26 14:39:17 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
 # Create your costume_stores migration here
+class CreateCostumeStore < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |cs|
+      cs.string :name
+      cs.string :location
+      cs.integer :costume_inventory
+      cs.integer :num_of_employees
+    end
+  end
+end
