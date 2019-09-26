de31a65b84c67b74e8f49857a2b288b1d7c29cc9
Austin Asbury <acasbury25@aol.com>
2019-09-26 14:40:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,6 +6,9 @@ class CreateCostumeStore < ActiveRecord::Migration[5.1]
       cs.string :location
       cs.integer :costume_inventory
       cs.integer :num_of_employees
+      cs.boolean :still_in_business
+      cs.datetime :opening_time
+      cs.datetime :closing_time
     end
   end
 end
